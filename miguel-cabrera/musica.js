f2eaa3117dfd898947c893149eccf788d70256a1
mike_tps <miguel.cabrera.landin@gmail.com>
2024-12-15 23:46:50 +0100

    feat: album pages

--- miguel-cabrera/musica.js
+++ miguel-cabrera/musica.js
@@ -1,6 +1,3 @@
-let lastStarTime = 0; // Tiempo de la última estrella
-const starInterval = 100; // Intervalo en milisegundos para crear estrellas
-
 // Importamos las imagenes y el sonido
 const images = document.querySelectorAll('.album');
 const sound = document.getElementById('wii');
@@ -16,29 +13,3 @@ images.forEach(image => {
         sound.pause(); // Pause sound when mouse leaves
     });
 });
-
-// Crear una estrella en el mouse
-document.addEventListener('mousemove', function(e) {
-    const currentTime = Date.now();
-    if (currentTime - lastStarTime > starInterval) {
-        crearEstrella(e.clientX, e.clientY);
-        lastStarTime = currentTime; // Actualiza el tiempo de la última estrella
-    }
-});
-
-// Crear una estrella
-function crearEstrella(x, y) {
-    const estrella = document.createElement('img');
-    estrella.src = 'media/img/star.png'; // Cambia esto por la ruta de tu imagen
-    estrella.classList.add('estrella');
-
-    estrella.style.left = `${x}px`;
-    estrella.style.top = `${y + window.scrollY}px`;
-
-    document.body.appendChild(estrella);
-
-    // Eliminar la estrella después de que caiga
-    estrella.addEventListener('animationend', function() {
-        estrella.remove();
-    });
-}
