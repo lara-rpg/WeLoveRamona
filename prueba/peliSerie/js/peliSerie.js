7f18928efe5b454310b956bc651cd06a48191258
SunDrake153 <alonsomail05@gmail.com>
2024-12-16 02:11:06 +0100

    yguiuiu

---
+++ prueba/peliSerie/js/peliSerie.js
@@ -0,0 +1,7 @@
+function socialMedia(){
+	alert("Error. Estas redes sociales todavía no han sido creadas, por favor, inténtalo próximamente.");
+}
+
+function proceso(){
+	alert("Proyecto en curso... Estreno de la página proximamente");
+}
