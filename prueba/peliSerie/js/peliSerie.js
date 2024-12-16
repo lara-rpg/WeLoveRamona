5dcced0d6121cb9f89ed87da500aff591b0bcd51
SunDrake153 <alonsomail05@gmail.com>
2024-12-16 02:16:38 +0100

    actualizacionFinal

--- prueba/peliSerie/js/peliSerie.js
+++
@@ -1,7 +0,0 @@
-function socialMedia(){
-	alert("Error. Estas redes sociales todavía no han sido creadas, por favor, inténtalo próximamente.");
-}
-
-function proceso(){
-	alert("Proyecto en curso... Estreno de la página proximamente");
-}
